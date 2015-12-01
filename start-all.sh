5d332a52302184ec3a424617c412d65e51af1cf7
morefreeze <morefreeze@gmail.com>
2015-12-01 13:15:25 +0800

    remove \n

--- start-all.sh
+++ start-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 $HBASE_PATH/bin/start-hbase.sh && \
-    echo "Sleep a while to wait hbase start completely... \n" && \
+    echo "Sleep a while to wait hbase start completely..." && \
     sleep 30 && $GOPATH/bin/tidb-server -store hbase -L error -path localhost/testdb
