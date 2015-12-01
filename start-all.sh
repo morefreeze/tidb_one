f05b992aba265d5dcdcbb0c703b7ef0a96cb2573
morefreeze <morefreeze@gmail.com>
2015-12-01 12:34:33 +0800

    change log level

--- start-all.sh
+++ start-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 $HBASE_PATH/bin/start-hbase.sh && \
     echo "Sleep a while to wait hbase start completely... \n" && \
-    sleep 30 && $GOPATH/bin/tidb-server -store hbase -path localhost/testdb
+    sleep 30 && $GOPATH/bin/tidb-server -store hbase -L error -path localhost/testdb
